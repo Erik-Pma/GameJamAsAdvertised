d1883aecc6d43608f1c142d7cbd0c42b742d3a95
Erik-Pma <56539093+Erik-Pma@users.noreply.github.com>
2026-01-08 21:10:43 -0800

    testing camera values

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -6,17 +6,24 @@ public class PlayerMovement : MonoBehaviour
     PlayerInput _playerInput;
     
     InputAction _moveAction;
+    InputAction _cameraAction;
+    
+    Transform _cameraTransform;
 
     [SerializeField] float _moveSpeed = 5.0f;
     private void Awake()
     {
         _playerInput = GetComponent<PlayerInput>();
         _moveAction = _playerInput.actions.FindAction("Move");
+        _cameraAction = _playerInput.actions.FindAction("Camera");
+        _cameraTransform = GetComponentInChildren<Transform>();
+
     }
 
     private void Update()
     {
         MovePlayer();
+        MoveCamera();
     }
 
     void MovePlayer()
@@ -24,5 +31,10 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log(_moveAction.ReadValue<Vector2>());
         Vector2 direction = _moveAction.ReadValue<Vector2>();
         transform.position += new Vector3(direction.x, 0, direction.y) * Time.deltaTime * _moveSpeed;
+    }
+
+    void MoveCamera()
+    {
+        Debug.Log(_cameraAction.ReadValue<Vector2>());
     }
 }
