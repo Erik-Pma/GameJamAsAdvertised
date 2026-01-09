80fce0a59328d4d0b8fdfc7989d95044ac41a207
Erik-Pma <56539093+Erik-Pma@users.noreply.github.com>
2026-01-09 09:54:35 -0800

    fixing collision

--- Assets/Scripts/Movement/CameraController.cs
+++ Assets/Scripts/Movement/CameraController.cs
@@ -1,45 +1,50 @@
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+namespace Movement
 {
-    public float minX = -60f;
-    public float maxX = 60f;
-
-    public float sensitivity;
-
-    public Camera cam;
-
-    float _rotY = 0f;
-    float _rotX = 0f;
-
-    PlayerMovement _move;
-    
-    private Vector2 _lookAmt;
-    // Start is called before the first frame update
-    void Start()
+    public class CameraController : MonoBehaviour
     {
-        //lock cursor and set it to false so you can move the screen more easily
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-        _move = GetComponent<PlayerMovement>();
-    }
+        public float minX = -60f;
+        public float maxX = 60f;
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        _lookAmt = _move.lookAction.ReadValue<Vector2>();
-        if (transform.position.y < -7)
+        public float sensitivity;
+
+        public Camera cam;
+
+        private float _rotY;
+        private float _rotX;
+
+        PlayerMovement _move;
+
+        private Vector2 _lookAmt;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
+            //lock cursor and set it to false so you can move the screen more easily
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            _move = GetComponent<PlayerMovement>();
         }
-        //get the camera and make it move to the mouse
-        _rotY += _lookAmt.x * sensitivity * Time.deltaTime;
-        _rotX += _lookAmt.y * sensitivity * Time.deltaTime;
-        //clamp the rotation to it does over rotate
-        _rotX = Mathf.Clamp(_rotX, minX, maxX);
 
-        transform.localEulerAngles = new Vector3(0, _rotY, 0);
-        cam.transform.localEulerAngles = new Vector3(-_rotX, 0, 0);
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            _lookAmt = _move.lookAction.ReadValue<Vector2>();
+            if (transform.position.y < -7)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+
+            //get the camera and make it move to the mouse
+            _rotY += _lookAmt.x * sensitivity * Time.deltaTime;
+            _rotX += _lookAmt.y * sensitivity * Time.deltaTime;
+            //clamp the rotation to it does over rotate
+            _rotX = Mathf.Clamp(_rotX, minX, maxX);
+
+            transform.localEulerAngles = new Vector3(0, _rotY, 0);
+            cam.transform.localEulerAngles = new Vector3(-_rotX, 0, 0);
+        }
     }
 }
--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -2,51 +2,56 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+namespace Movement
 {
-    [Header("InputMapping")]
-    public InputActionAsset actionAsset;
-    
-    private InputAction _moveAction;
-    [HideInInspector]
-    public InputAction lookAction;
-    
-    private Vector2 _moveAmt;
-    private float _xRotation;
-    [Header("values")]    
-    public float moveSpeed = 5.0f;
-    private void OnEnable()
+    public class PlayerMovement : MonoBehaviour
     {
-        actionAsset.FindActionMap("Movement").Enable();
-    }
+        [Header("InputMapping")] public InputActionAsset actionAsset;
 
-    private void OnDisable()
-    {
-        actionAsset.FindActionMap("Movement").Disable();
-    }
-    private void Awake()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        _moveAction = actionAsset.FindAction("Move");
-        lookAction = actionAsset.FindAction("Look");
+        private InputAction _moveAction;
+        [HideInInspector] public InputAction lookAction;
+
+        private Vector2 _moveAmt;
+        private float _xRotation;
+        [Header("values")] public float moveSpeed = 5.0f;
+        [Header("CharacterController")] public CharacterController controller;
+
+        private void OnEnable()
+        {
+            actionAsset.FindActionMap("Movement").Enable();
+        }
+
+        private void OnDisable()
+        {
+            actionAsset.FindActionMap("Movement").Disable();
+        }
+
+        private void Awake()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            _moveAction = actionAsset.FindAction("Move");
+            lookAction = actionAsset.FindAction("Look");
 
 
-    }
-    private void Update()
-    {
-        _moveAmt = _moveAction.ReadValue<Vector2>();
+        }
 
-    }
+        private void Update()
+        {
+            _moveAmt = _moveAction.ReadValue<Vector2>();
 
-    private void FixedUpdate()
-    {
-        Movement();
-    }
+        }
 
-    private void Movement()
-    {
-        //Debug.Log(_moveAmt);
-        Vector3 movementVector = moveSpeed * Time.deltaTime *(transform.forward * _moveAmt.y + transform.right * _moveAmt.x) ;
-        transform.position += movementVector;
+        private void FixedUpdate()
+        {
+            Movement();
+        }
+
+        private void Movement()
+        {
+            //Debug.Log(_moveAmt);
+            Vector3 movementVector = moveSpeed * Time.deltaTime *
+                                     (transform.forward * _moveAmt.y + transform.right * _moveAmt.x);
+            controller.Move(movementVector);
+        }
     }
 }
